4e3343b0cc25e0b7c5ad118795bf45673171b785
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2022-02-02 15:20:14 -0300

    Implementaçao de verificação de cep e busca em cache

--- api/server/controllers/viaCepController.js
+++ api/server/controllers/viaCepController.js
@@ -19,31 +19,27 @@ class viaCepController {
         }
       );
 
-      const buscaCache = await myCache.get("cep")
+      const buscaCache = await myCache.get("cep");
 
-      if (buscaCache == undefined) {
-
-        const busca = await buscaCep(req.params.id);
-
-        if (busca.erro) {
-          request.setError("Cep inexistente!", 400);
-          return request.send(res);
-        }
-          
-          await myCache.set("cep", busca, 300);
-        
-        if (busca) {
-          request.setSuccess(200, "Cep consultado com sucesso", busca);
-        } else request.setError("Não foi possível consultar o Cep!", 400);
-        return request.send(res);
-        
-      }else{
+      if (buscaCache && buscaCache.cep.replace('-','') == req.params.id) {
         request.setSuccess(200, "Cep consultado via cache", buscaCache);
         return request.send(res);
       }
+
+      const busca = await buscaCep(req.params.id);
+
+      if (busca.erro) {
+        request.setError("Cep inexistente!", 400);
+        return request.send(res);
+      }
+
+      await myCache.set("cep", busca, 300);
       
+      request.setSuccess(200, "Cep consultado com sucesso", busca);
+      return request.send(res);
+
     } catch (error) {
-      request.setError(error);
+      request.setError("Não foi possível consultar o Cep!", 400);
       return request.send(res);
     }
   }
