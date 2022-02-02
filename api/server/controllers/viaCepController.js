d78f34e095045fb519186f46f7417a391db18269
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2022-02-02 14:01:54 -0300

    Ajustes no controller

--- api/server/controllers/viaCepController.js
+++ api/server/controllers/viaCepController.js
@@ -1,10 +1,10 @@
 import { Request } from "../util";
 import buscaCep from "../services/viaCepService";
 import { buscaCepValidation } from "../validations";
-import NodeCache  from "node-cache" 
+import NodeCache from "node-cache"
 
 const request = new Request();
-const myCache = new NodeCache({ stdTTL: 300, checkperiod: 300 });
+const myCache = new NodeCache();
 
 class viaCepController {
 
@@ -18,27 +18,30 @@ class viaCepController {
           abortEarly: false,
         }
       );
-  
-      const buscaCache = myCache.get("cep")
 
-      if (!buscaCache){
-        const buscaPeloCache = await buscaCep(req.params.id);
-        return buscaPeloCache
-      }
+      const buscaCache = await myCache.get("cep")
 
-      const busca = await buscaCep(buscaCache);
-      await myCache.set( "cep", busca, 300 );
+      if (buscaCache == undefined) {
 
-      if (busca.erro) {
-        request.setError("Cep inexistente!", 400);
+        const busca = await buscaCep(req.params.id);
+
+        if (busca.erro) {
+          request.setError("Cep inexistente!", 400);
+          return request.send(res);
+        }
+          
+          await myCache.set("cep", busca, 300);
+        
+        if (busca) {
+          request.setSuccess(200, "Cep consultado com sucesso", busca);
+        } else request.setError("Não foi possível consultar o Cep!", 400);
+        return request.send(res);
+        
+      }else{
+        request.setSuccess(200, "Cep consultado via cache", buscaCache);
         return request.send(res);
       }
-
-      if (busca) {
-        request.setSuccess(200, "Cep consultado com sucesso", busca);
-      } else request.setError("Não foi possível consultar o Cep!", 400);
-
-      return request.send(res);
+      
     } catch (error) {
       request.setError(error);
       return request.send(res);
